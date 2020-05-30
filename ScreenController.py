b50cc878e56b144362e95b559d35f76eed1d1df7
Izolentiy <nima.munkonov@gmail.com>
2020-05-30 23:17:10 +0800

    Minimal functionality

--- ScreenController.py
+++ ScreenController.py
@@ -1,7 +1,6 @@
 from PyQt5 import QtWidgets
 
-from screens.InOutScreen import InOutScreen
-from screens.WelcomeScreen import WelcomeScreen
+from screens.Screen import Screen
 
 
 class ScreenController(QtWidgets.QMainWindow):
@@ -17,16 +16,33 @@ class ScreenController(QtWidgets.QMainWindow):
         self.resize(500, 250)
 
         # Создание экранов
-        welcomeScreen = WelcomeScreen()
-        inoutScreen = InOutScreen()
+        self.screen = Screen()
 
-        self.set_screen(welcomeScreen)  # установка приветственного экрана
+        self.set_screen(self.screen)  # установка экрана
         self.show()  # метод для отображения главного окна
 
     # Манипуляция с экранами
     def set_screen(self, screen):
         screen.show_ui(self)
+        screen.calculate.clicked.connect(self.button_processor)
 
     # Обработка нажатий
     def button_processor(self):
-        pass
+        if self.screen.weight_field.text() is '' or self.screen.height_field.text() is '' or self.screen.age_field.text() is '':
+            print('Enter data')
+        else:
+            sex = self.screen.sex_field.currentText()
+            height = float(self.screen.height_field.text())
+            age = float(self.screen.age_field.text())
+            weight = float(self.screen.weight_field.text())
+
+            self.calculate(height, weight, age, sex)
+
+    def calculate(self, height, weight, age, sex):
+        if sex == 'М':
+            result = 66 + (13.7 * weight) + (5 * height) - (6.76 * age)
+        elif sex == 'Ж':
+            result = 665 + (9.6 * weight) + (1.8 * height) - (4.7 * age)
+        else:
+            result = '???'
+        self.screen.result.setText(str(result) + ' ккал')
--- screens/Screen.py
+++ screens/Screen.py
@@ -3,7 +3,7 @@ from PyQt5 import QtWidgets, QtCore, QtGui
 from screens.BaseScreen import BaseScreen
 
 
-class WelcomeScreen(BaseScreen):
+class Screen(BaseScreen):
 
     def init_content(self, layout):
         # Шрифт
@@ -24,14 +24,14 @@ class WelcomeScreen(BaseScreen):
         name_field.setAlignment(QtCore.Qt.AlignCenter)
 
         # Кнопка для ввода
-        start = QtWidgets.QPushButton()
-        start.setObjectName('calculate')
-        start.setText('ПОСЧИТАТЬ')
+        calculate = QtWidgets.QPushButton()
+        calculate.setObjectName('calculate')
+        calculate.setText('ПОСЧИТАТЬ')
 
         # Создание атрибутов данных для доступа к ним извне
         self.welcome = welcome
         self.name_field = name_field
-        self.start = start
+        self.calculate = calculate
 
         # Добавление элементов на экран
         layout.addStretch(1)
@@ -42,42 +42,50 @@ class WelcomeScreen(BaseScreen):
         layout.addStretch(1)
 
         # Описания
+        weight_tip = QtWidgets.QLabel()
         sex_tip = QtWidgets.QLabel()
         age_tip = QtWidgets.QLabel()
         height_tip = QtWidgets.QLabel()
         life_tip = QtWidgets.QLabel()
 
+        weight_tip.setObjectName('weight_tip')
         sex_tip.setObjectName('sex_tip')
         age_tip.setObjectName('age_tip')
         height_tip.setObjectName('height_tip')
         life_tip.setObjectName('life_tip')
 
-        sex_tip.setText('Ваш пол:')
-        age_tip.setText('Ваш возраст:')
-        height_tip.setText('Ваш рост:')
-        life_tip.setText('Ваш образ жизни:')
+        weight_tip.setText('Ваш текущий вес: ')
+        sex_tip.setText('Ваш пол: ')
+        age_tip.setText('Ваш возраст: ')
+        height_tip.setText('Ваш рост: ')
+        life_tip.setText('Ваш образ жизни: ')
 
         # Поля ввода
+        weight_field = QtWidgets.QLineEdit()
         sex_field = QtWidgets.QComboBox()
         age_field = QtWidgets.QLineEdit()
         height_field = QtWidgets.QLineEdit()
         life_field = QtWidgets.QComboBox()
 
+        weight_field.setObjectName('weight_field')
         sex_field.setObjectName('sex_field')
         age_field.setObjectName('age_field')
         height_field.setObjectName('height_field')
         life_field.setObjectName('life_field')
 
+        weight_field.setPlaceholderText('60 кг')
+        weight_field.setValidator(QtGui.QIntValidator(40, 200))
+
         # Заполнение комбо-бокса с полом
-        sex_field.addItem('M')
+        sex_field.addItem('М')
         sex_field.addItem('Ж')
         sex_field.addItem('Т')
 
         age_field.setPlaceholderText('20')
-        # age_field.setValidator(QtGui.QIntValidator(15, 100))
+        age_field.setValidator(QtGui.QIntValidator(15, 100))
 
         height_field.setPlaceholderText('175 см')
-        # height_field.setValidator(QtGui.QIntValidator(150, 250))
+        height_field.setValidator(QtGui.QIntValidator(150, 250))
 
         life_field.addItem('Мебель')
         life_field.addItem('Сидячая работа')
@@ -85,7 +93,6 @@ class WelcomeScreen(BaseScreen):
         life_field.addItem('Физически тяжёлая работа')
         life_field.addItem('Грузчик 24/7')
 
-        # layout = QtWidgets.QHBoxLayout()  # основной макет
         in_lt = QtWidgets.QGridLayout()  # макет для ввода
         out_lt = QtWidgets.QHBoxLayout()  # макет для вывода
 
@@ -100,11 +107,13 @@ class WelcomeScreen(BaseScreen):
 
         # Добавление всех элементов на макеты
 
+        self.weight_tip = weight_tip
         self.sex_tip = sex_tip
         self.age_tip = age_tip
         self.height_tip = height_tip
         self.life_tip = life_tip
 
+        self.weight_field = weight_field
         self.sex_field = sex_field
         self.age_field = age_field
         self.height_field = height_field
@@ -113,14 +122,16 @@ class WelcomeScreen(BaseScreen):
         self.out_tip = out_tip
         self.result = result
 
-        in_lt.addWidget(self.sex_tip, 0, 0)
-        in_lt.addWidget(self.sex_field, 0, 1)
-        in_lt.addWidget(self.age_tip, 1, 0)
-        in_lt.addWidget(self.age_field, 1, 1)
-        in_lt.addWidget(self.height_tip, 2, 0)
-        in_lt.addWidget(self.height_field, 2, 1)
-        in_lt.addWidget(self.life_tip, 3, 0)
-        in_lt.addWidget(self.life_field, 3, 1)
+        in_lt.addWidget(self.weight_tip, 0, 0)
+        in_lt.addWidget(self.weight_field, 0, 1)
+        in_lt.addWidget(self.sex_tip, 1, 0)
+        in_lt.addWidget(self.sex_field, 1, 1)
+        in_lt.addWidget(self.age_tip, 2, 0)
+        in_lt.addWidget(self.age_field, 2, 1)
+        in_lt.addWidget(self.height_tip, 3, 0)
+        in_lt.addWidget(self.height_field, 3, 1)
+        in_lt.addWidget(self.life_tip, 4, 0)
+        in_lt.addWidget(self.life_field, 4, 1)
 
         out_lt.addWidget(self.out_tip)
         out_lt.addWidget(self.result)
@@ -128,7 +139,7 @@ class WelcomeScreen(BaseScreen):
         layout.addStretch(1)
         layout.addLayout(in_lt)
         layout.addSpacing(50)
-        layout.addWidget(self.start)
+        layout.addWidget(self.calculate)
         layout.addSpacing(15)
         layout.addLayout(out_lt)
         layout.addStretch(1)
